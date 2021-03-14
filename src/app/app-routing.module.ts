b9a8ba3c22603ccf7f8c044af4a5ff7d23f44d60
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-03-14 11:05:40 +0100

    Modules added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,22 @@ const routes: Routes = [
     loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
   },
   {
+    path: 'admin-candidate',
+    loadChildren: () => import('./admin-pages/admin-candidate/admin-candidate.module').then(m => m.AdminCandidateModule)
+  },
+  {
+    path: 'admin-instructor',
+    loadChildren: () => import('./admin-pages/admin-instructor/admin-instructor.module').then(m => m.AdminInstructorModule)
+  },
+  {
+    path: 'admin-notification',
+    loadChildren: () => import('./admin-pages/admin-notification/admin-notification.module').then(m => m.AdminNotificationModule)
+  },
+  {
+    path: 'admin-vehicle',
+    loadChildren: () => import('./admin-pages/admin-vehicle/admin-vehicle.module').then(m => m.AdminVehicleModule)
+  },
+  {
     path: '**',
     component: MainComponent
   }
