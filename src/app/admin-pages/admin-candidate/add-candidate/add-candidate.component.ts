e9e8c3e2ff4d266dc8567e9e37f1d6303f3fc91b
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-04-04 17:20:49 +0200

    New inputs added

--- src/app/admin-pages/admin-candidate/add-candidate/add-candidate.component.ts
+++ src/app/admin-pages/admin-candidate/add-candidate/add-candidate.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-add-candidate',
   templateUrl: './add-candidate.component.html',
-  styleUrls: ['./add-candidate.component.scss']
+  styleUrls: ['./add-candidate.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddCandidateComponent implements OnInit {
 
