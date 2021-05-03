f62ef41d7c13ecd18a9f15a6e5740da3231784b0
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-05-03 21:38:48 +0200

    Candidate page updated

--- src/app/admin-pages/admin-candidate/edit-candidate/edit-candidate.component.ts
+++ src/app/admin-pages/admin-candidate/edit-candidate/edit-candidate.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-edit-candidate',
   templateUrl: './edit-candidate.component.html',
-  styleUrls: ['./edit-candidate.component.scss']
+  styleUrls: ['./edit-candidate.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EditCandidateComponent implements OnInit {
 
