801b89b0dc2771baae00162eb2747c6559178647
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-05-09 19:13:49 +0200

    Add and Edit vehicle pages updated

--- src/app/admin-pages/admin-vehicle/add-vehicle/add-vehicle.component.ts
+++ src/app/admin-pages/admin-vehicle/add-vehicle/add-vehicle.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-add-vehicle',
   templateUrl: './add-vehicle.component.html',
-  styleUrls: ['./add-vehicle.component.scss']
+  styleUrls: ['./add-vehicle.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddVehicleComponent implements OnInit {
 
--- src/app/admin-pages/admin-vehicle/edit-vehicle/edit-vehicle.component.ts
+++ src/app/admin-pages/admin-vehicle/edit-vehicle/edit-vehicle.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-edit-vehicle',
   templateUrl: './edit-vehicle.component.html',
-  styleUrls: ['./edit-vehicle.component.scss']
+  styleUrls: ['./edit-vehicle.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EditVehicleComponent implements OnInit {
 
