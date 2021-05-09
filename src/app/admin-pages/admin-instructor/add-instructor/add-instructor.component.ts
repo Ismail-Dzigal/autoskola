3de8d5a6f52858229d75eea588456137e7c23bf2
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-05-09 17:25:25 +0200

    Add and edit instructor pages updated

--- src/app/admin-pages/admin-instructor/add-instructor/add-instructor.component.ts
+++ src/app/admin-pages/admin-instructor/add-instructor/add-instructor.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-add-instructor',
   templateUrl: './add-instructor.component.html',
-  styleUrls: ['./add-instructor.component.scss']
+  styleUrls: ['./add-instructor.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddInstructorComponent implements OnInit {
 
--- src/app/admin-pages/admin-instructor/edit-instructor/edit-instructor.component.ts
+++ src/app/admin-pages/admin-instructor/edit-instructor/edit-instructor.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-edit-instructor',
   templateUrl: './edit-instructor.component.html',
-  styleUrls: ['./edit-instructor.component.scss']
+  styleUrls: ['./edit-instructor.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class EditInstructorComponent implements OnInit {
 
