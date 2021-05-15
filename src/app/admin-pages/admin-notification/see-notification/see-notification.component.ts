22e1f9ec6132380dbe6c387e8dd873153039286b
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-05-15 17:21:05 +0200

    New files added

--- src/app/admin-pages/admin-notification/see-notification/see-notification.component.ts
+++ src/app/admin-pages/admin-notification/see-notification/see-notification.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-see-notification',
   templateUrl: './see-notification.component.html',
-  styleUrls: ['./see-notification.component.scss']
+  styleUrls: ['./see-notification.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SeeNotificationComponent implements OnInit {
 
