06f4c10b18b0ce062bd329adec38ccd109109bb0
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-05-23 21:05:29 +0200

    New pages implemented

--- src/app/instructor-pages/notifications-page/notifications-page.component.ts
+++ src/app/instructor-pages/notifications-page/notifications-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-notifications-page',
   templateUrl: './notifications-page.component.html',
-  styleUrls: ['./notifications-page.component.scss']
+  styleUrls: ['./notifications-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NotificationsPageComponent implements OnInit {
 
