0cc3c8372d8612d211699f72864770f2f6073b63
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-06-05 21:28:03 +0200

    Candidate lectures report added

--- src/app/instructor-pages/candidate-page/candidate-page.component.ts
+++ src/app/instructor-pages/candidate-page/candidate-page.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-candidate-page',
   templateUrl: './candidate-page.component.html',
-  styleUrls: ['./candidate-page.component.scss']
+  styleUrls: ['./candidate-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CandidatePageComponent implements OnInit {
+  paginationInfo: any;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  previousPage: number;
+  totalCount: number = 3;
+  totalPages: number = 1;
 
   constructor() { }
 
