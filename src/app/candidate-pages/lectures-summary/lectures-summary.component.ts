c3748c5575a8f86e7d92077315c8e673a36a37d1
Ismail-Dzigal <ismail_dzigal@yahoo.com>
2021-08-11 11:51:18 +0200

    New candidate pages finished

--- src/app/candidate-pages/lectures-summary/lectures-summary.component.ts
+++ src/app/candidate-pages/lectures-summary/lectures-summary.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-lectures-summary',
   templateUrl: './lectures-summary.component.html',
-  styleUrls: ['./lectures-summary.component.scss']
+  styleUrls: ['./lectures-summary.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LecturesSummaryComponent implements OnInit {
+  paginationInfo: any;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  previousPage: number;
+  totalCount: number = 3;
+  totalPages: number = 1;
 
   constructor() { }
 
--- src/app/candidate-pages/lectures-times/lectures-times.component.ts
+++ src/app/candidate-pages/lectures-times/lectures-times.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-lectures-times',
   templateUrl: './lectures-times.component.html',
-  styleUrls: ['./lectures-times.component.scss']
+  styleUrls: ['./lectures-times.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LecturesTimesComponent implements OnInit {
 
